097b8b74885d2243e447e21365992ef21a0971ae
Isa del Águila <imaguila@ual.es>
2026-08-01 22:41:55 +0200

    asd

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,7 +1,11 @@
 # plugins/__init__.py
 
 from .nrp_plugin import NRPPlugin
+from .aerospace_plugin import AerospacePlugin
 
 PLUGIN_REGISTRY = {
+
     "nrp": NRPPlugin,
+
+    "aerospace": AerospacePlugin
 }
