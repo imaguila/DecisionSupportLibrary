80fc99e304c0c91ed87b2a70445a275b65988114
Isa del Águila <imaguila@ual.es>
2026-08-02 15:13:50 +0200

    j

--- core/framing.py
+++ core/framing.py
@@ -19,27 +19,31 @@ def apply_framing(dataset):
         expanded=False
     ):
 
-
         # ----------------------------------
         # Dimension filters
         # ----------------------------------
 
         for metric in dimensions:
 
-            if metric not in filtered_df.columns:
+            if metric not in df.columns:
                 continue
+
             if not pd.api.types.is_numeric_dtype(
-                filtered_df[metric]
+                df[metric]
             ):
                 continue
+
             min_v = float(
                 df[metric].min()
             )
+
             max_v = float(
                 df[metric].max()
             )
+
             if min_v == max_v:
                 continue
+
             selected_range = st.slider(
                 metric,
                 min_v,
@@ -48,27 +52,37 @@ def apply_framing(dataset):
                 key=f"framing_{metric}"
             )
 
-            EPS = 1e-6
+            # Si el slider está abierto completamente,
+            # no aplicar ningún filtro.
+            if (
+                abs(selected_range[0] - min_v) < 1e-6
+                and
+                abs(selected_range[1] - max_v) < 1e-6
+            ):
+                continue
 
             filtered_df = filtered_df[
                 (
                     filtered_df[metric]
-                    >= selected_range[0] - EPS
+                    >= selected_range[0]
                 )
                 &
                 (
                     filtered_df[metric]
-                    <= selected_range[1] + EPS
+                    <= selected_range[1]
                 )
             ]
 
         # ----------------------------------
         # Framing summary
         # ----------------------------------
+
         total_solutions = len(df)
+
         remaining_solutions = len(
             filtered_df
         )
+
         ratio = (
             remaining_solutions
             /
@@ -96,17 +110,24 @@ def apply_framing(dataset):
         )
 
         # ----------------------------------
-        # Reset framing TODO
+        # Reset framing
         # ----------------------------------
 
         if st.button(
             "🔄 Reset Framing",
             use_container_width=True
         ):
-            for key in list(st.session_state.keys()):
-                if key.startswith("framing_"):
+
+            for key in list(
+                st.session_state.keys()
+            ):
+
+                if key.startswith(
+                    "framing_"
+                ):
 
                     del st.session_state[key]
+
             st.rerun()
 
     return filtered_df
